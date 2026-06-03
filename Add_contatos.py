ade187d5a832ef7d6c8ed59f8047ca3805d4326d
Samuel Lima <samuel.aguiar0106@gmail.com>
2026-06-03 11:29:37 -0300

    Função Adiciona Dados

---
+++ Add_contatos.py
@@ -0,0 +1,16 @@
+#FUNÇÃO QUE ADICIONA OS CONTATOS
+quant =  int(input("Quantos contatos deseja adicionar?: "))
+dados_total = list()
+def add():
+    for c in range(quant):
+        lista_contato = list()
+        print('=-'*20)
+        nome = input("Qual o nome do contato?: ")
+        tele = input("Qual o telefone do contato?: ")
+        email = input("Qual o e-mail do contato?: ")
+        print('=-' * 20)
+        lista_contato.append(nome)
+        lista_contato.append(tele)
+        lista_contato.append(email)
+        dados_total.append(lista_contato)
+
